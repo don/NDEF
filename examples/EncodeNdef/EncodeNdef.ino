a0789326405fbb0c345a5fb8c862130df4a2fdfb
Don Coleman <dcoleman@chariotsolutions.com>
2013-02-07 18:30:06 -0500

    update example

--- examples/EncodeNdef/EncodeNdef.ino
+++ examples/EncodeNdef/EncodeNdef.ino
@@ -4,8 +4,9 @@ void setup() {
   Serial.begin(115200);
   Serial.println("\n");
   
+  // create a record the long way  
   NdefRecord* r = new NdefRecord();
-  r->setTnf(0x2);  // TNF Mime Media
+  r->setTnf(TNF_MIME_MEDIA); 
   
   String type = String("foo");
   String payload = String("17");
@@ -17,19 +18,30 @@ void setup() {
   byte payloadBytes[payload.length() + 1];
   payload.getBytes(payloadBytes, sizeof(payloadBytes));
   r->setPayload(payloadBytes, payload.length());
-  
+
+  // create a message and add record
   NdefMessage* message = new NdefMessage();
   message->add(*r);
+  
+  // a more records using the helper methods
   message->addMimeMediaRecord("foo", "17");
   message->addMimeMediaRecord("foo", payloadBytes, payload.length());
+  message->addTextRecord("foo");
+  message->addTextRecord("foo", "en");
+  message->addEmptyRecord();
+  message->addUriRecord("http://example.com");
+
+  // verify the contents
   message->print();
   
+  // create an array to hold the encoded message
   uint8_t encoded[message->getEncodedSize()];
+ 
   message->encode(encoded);
   // TODO need NdefUtil::PrintHex(encoded)
   
   Serial.println("\nExpecting");
-  Serial.println("0xd2 0x03 0x02 0x66 0x6f 0x6f 0x31 0x37");
+  Serial.println("0xd2 0x03 0x02 0x66 0x6f 0x6f 0x31 0x37...");
 
 }
 
