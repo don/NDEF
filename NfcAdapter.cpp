262e72c7c87b2b5e46c0a4b194328211da953a23
Don Coleman <dcoleman@chariotsolutions.com>
2013-02-20 17:41:59 -0500

    more verbose output and configure the reader

--- NfcAdapter.cpp
+++ NfcAdapter.cpp
@@ -16,13 +16,26 @@ NfcAdapter::~NfcAdapter(void)
 void NfcAdapter::begin()
 {
 	shield->begin();
+
+  uint32_t versiondata = shield->getFirmwareVersion();
+  if (! versiondata) {
+    Serial.print("Didn't find PN53x board");
+    while (1); // halt
+  }
+  // Got ok data, print it out!
+  Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX); 
+  Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
+  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
+  
+  // configure board to read RFID tags
+  shield->SAMConfig();
 }
 
 boolean NfcAdapter::tagPresent()
 {
 	uint8_t success;
 	// reset uid and length
-  uid = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+  //uid = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
   uidLength = 0;                    // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
 
 	success = shield->readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
