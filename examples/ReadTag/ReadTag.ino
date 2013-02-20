b7f9988f2f7380fe71b55ebaf4ce2867f689084d
Don Coleman <dcoleman@chariotsolutions.com>
2013-02-20 17:42:09 -0500

    simplified examples

--- examples/ReadTag/ReadTag.ino
+++ examples/ReadTag/ReadTag.ino
@@ -1,70 +1,21 @@
 #include <Wire.h>
 #include <Adafruit_NFCShield_I2C.h>
-#include <Ndef.h>
-#include <MifareClassic.h>
-#include <MifareUltralight.h>
+#include <NfcAdapter.h>
 
-#define IRQ   (2)
-#define RESET (3)  // Not connected by default on the NFC Shield
-
-Adafruit_NFCShield_I2C nfc(IRQ, RESET);
+NfcAdapter nfc = NfcAdapter();
 
 void setup(void) {
   Serial.begin(115200);
-  Serial.println("Hello!");
-
+  Serial.println("NDEF Reader");
   nfc.begin();
-
-  uint32_t versiondata = nfc.getFirmwareVersion();
-  if (! versiondata) {
-    Serial.print("Didn't find PN53x board");
-    while (1); // halt
-  }
-  // Got ok data, print it out!
-  Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX); 
-  Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
-  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
-  
-  // configure board to read RFID tags
-  nfc.SAMConfig();
-  
-  Serial.println("Waiting for an ISO14443A Card ...");
 }
 
 void loop(void) {
-  uint8_t success;
-  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
-  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
-    
-  // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
-  // 'uid' will be populated with the UID, and uidLength will indicate
-  // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
-  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
-  
-  if (success) {
-    // Display some basic information about the card
-    Serial.println("Found an ISO14443A card");
-    Serial.print("  UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
-    Serial.print("  UID Value: ");
-    nfc.PrintHex(uid, uidLength);
-    Serial.println("");
-    
-    if (uidLength == 4)
-    {
-      // We probably have a Mifare Classic card ... 
-      Serial.println("Seems to be a Mifare Classic card (4 byte UID)");
-	  
-      NdefMessage ndefMessage = read(nfc, uid, uidLength);
-      ndefMessage.print();
-              
-      delay(5000);      
-    }
-    else
-    {
-      NdefMessage ndefMessage = readMifareUltralight(nfc);
-      ndefMessage.print();
-      delay(5000);
-    }
-    
+  Serial.println("\nScan a NFC tag");
+  if (nfc.tagPresent())
+  {
+    NdefMessage message = nfc.read();
+    message.print();
   }
+  delay(5000);
 }
--- examples/WriteTag/WriteTag.ino
+++ examples/WriteTag/WriteTag.ino
@@ -1,82 +1,22 @@
-//
-// Write "Hello, world!" in a plain text NDEF message
-// Tag must be a Mifare Classic with a 4 byte ID
-// Tested with Samsung TecTile
-//
 #include <Wire.h>
 #include <Adafruit_NFCShield_I2C.h>
-#include <Ndef.h>
-#include <MifareClassic.h>
+#include <NfcAdapter.h>
 
-#define IRQ   (2)
-#define RESET (3)  // Not connected by default on the NFC Shield
+NfcAdapter nfc = NfcAdapter();
 
-Adafruit_NFCShield_I2C nfc(IRQ, RESET);
-
-void setup(void) {
+void setup() {
   Serial.begin(115200);
-  Serial.println("Hello!");
-
+  Serial.println("NDEF Writer");
   nfc.begin();
-
-  uint32_t versiondata = nfc.getFirmwareVersion();
-  if (! versiondata) {
-    Serial.print("Didn't find PN53x board");
-    while (1); // halt
-  }
-  // Got ok data, print it out!
-  Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX); 
-  Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
-  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
-  
-  // configure board to read RFID tags
-  nfc.SAMConfig();
-  
-  Serial.println("Waiting for an ISO14443A Card ...");
 }
 
-void loop(void) {
-  uint8_t success;
-  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
-  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
-    
-  // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
-  // 'uid' will be populated with the UID, and uidLength will indicate
-  // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
-  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
-  
-  if (success) {
-    // Display some basic information about the card
-    Serial.println("Found an ISO14443A card");
-    Serial.print("  UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
-    Serial.print("  UID Value: ");
-    nfc.PrintHex(uid, uidLength);
-    Serial.println("");
-    
-    if (uidLength == 4)
-    {
-      // We probably have a Mifare Classic card ... 
-      Serial.println("Seems to be a Mifare Classic card (4 byte UID)");
-	  
-      NdefMessage* m = new NdefMessage();
-      //m->addTextRecord("Hello, World");
-      m->addUriRecord("http://arduino.cc");
-      //m->addEmptyRecord();
-      //m->addTextRecord("Goodbye, World");
-      
-      m->print();
-
-      write(nfc, *m, uid, uidLength);
-
-      Serial.println("\nTry and read the tag with your phone.");
-              
-      delay(5000);      
-    }
-    else
-    {
-      Serial.println("This sketch only supports Mifare Classic tags with 4 byte UIDs");
-      delay(5000);
-    }
-    
+void loop() {
+  Serial.println("\nPlace a formatted Mifare Classic NFC tag on the reader.");
+  if (nfc.tagPresent()) {
+    NdefMessage message = NdefMessage();
+    message.addTextRecord("Hello, Arduino!");
+    nfc.write(message);
+    Serial.println("Try reading this tag with your phone.");  
   }
+  delay(5000);
 }
