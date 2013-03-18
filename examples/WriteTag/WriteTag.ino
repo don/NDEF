30d04fe24c0eae5acf0fd0ea616d395a3dfba503
Don Coleman <dcoleman@chariotsolutions.com>
2013-03-18 00:38:47 -0400

    write uri record since it is easier to read on any phone. indented file so diff looks bigger

--- examples/WriteTag/WriteTag.ino
+++ examples/WriteTag/WriteTag.ino
@@ -5,18 +5,18 @@
 NfcAdapter nfc = NfcAdapter();
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("NDEF Writer");
-  nfc.begin();
+      Serial.begin(115200);
+      Serial.println("NDEF Writer");
+      nfc.begin();
 }
 
 void loop() {
-  Serial.println("\nPlace a formatted Mifare Classic NFC tag on the reader.");
-  if (nfc.tagPresent()) {
-    NdefMessage message = NdefMessage();
-    message.addTextRecord("Hello, Arduino!");
-    nfc.write(message);
-    Serial.println("Try reading this tag with your phone.");  
-  }
-  delay(5000);
+    Serial.println("\nPlace a formatted Mifare Classic NFC tag on the reader.");
+    if (nfc.tagPresent()) {
+        NdefMessage message = NdefMessage();
+        message.addUriRecord("http://arduino.cc");
+        nfc.write(message);
+        Serial.println("Try reading this tag with your phone.");
+    }
+    delay(5000);
 }
