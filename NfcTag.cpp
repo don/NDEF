790b2839e257a23be5522a528de0008dd1946988
Don Coleman <dcoleman@chariotsolutions.com>
2013-03-18 00:41:56 -0400

    tabs to spaces

--- NfcTag.cpp
+++ NfcTag.cpp
@@ -2,98 +2,98 @@
 
 NfcTag::NfcTag()
 {
-	_uid = 0;
-	_uidLength = 0;
-	_tagType = "Unknown";
-	_ndefMessage = (NdefMessage*)NULL;	
+    _uid = 0;
+    _uidLength = 0;
+    _tagType = "Unknown";
+    _ndefMessage = (NdefMessage*)NULL;  
 }
 
 NfcTag::NfcTag(uint8_t* uid, uint8_t uidLength)
 {
-	_uid = uid;
-	_uidLength = uidLength;
-	_tagType = "Unknown";
-	_ndefMessage = (NdefMessage*)NULL;	
+    _uid = uid;
+    _uidLength = uidLength;
+    _tagType = "Unknown";
+    _ndefMessage = (NdefMessage*)NULL;  
 }
 
 NfcTag::NfcTag(uint8_t* uid, uint8_t uidLength, String tagType)
 {
-	_uid = uid;
-	_uidLength = uidLength;
-	_tagType = tagType;
-	_ndefMessage = (NdefMessage*)NULL;	
+    _uid = uid;
+    _uidLength = uidLength;
+    _tagType = tagType;
+    _ndefMessage = (NdefMessage*)NULL;  
 }
 
 NfcTag::NfcTag(uint8_t* uid, uint8_t uidLength, String tagType, NdefMessage& ndefMessage)
 {
-	_uid = uid;
-	_uidLength = uidLength;
-	_tagType = tagType;
-	_ndefMessage = new NdefMessage(ndefMessage);	
+    _uid = uid;
+    _uidLength = uidLength;
+    _tagType = tagType;
+    _ndefMessage = new NdefMessage(ndefMessage);    
 }
 
 // I don't like this version, but it will use less memory
 NfcTag::NfcTag(uint8_t* uid, uint8_t uidLength, String tagType, const uint8_t * ndefData, const int ndefDataLength)
 {
-	_uid = uid;
-	_uidLength = uidLength;
-	_tagType = tagType;
-	_ndefMessage = new NdefMessage(ndefData, ndefDataLength);	
+    _uid = uid;
+    _uidLength = uidLength;
+    _tagType = tagType;
+    _ndefMessage = new NdefMessage(ndefData, ndefDataLength);   
 }
 
 NfcTag::~NfcTag()
 {
-	delete _ndefMessage;
+    delete _ndefMessage;
 }
 
 NfcTag& NfcTag::operator=(const NfcTag& rhs)
 {
-	if (this != &rhs)
+    if (this != &rhs)
     {
-    	delete _ndefMessage;
-    	_uid = rhs._uid;
-		_uidLength = rhs._uidLength;
-		_tagType = rhs._tagType;
-		// TODO do I need a copy here?
-		_ndefMessage = rhs._ndefMessage;	
+        delete _ndefMessage;
+        _uid = rhs._uid;
+        _uidLength = rhs._uidLength;
+        _tagType = rhs._tagType;
+        // TODO do I need a copy here?
+        _ndefMessage = rhs._ndefMessage;    
     }
     return *this;
 }
 
 uint8_t NfcTag::getUidLength()
 {
-	return _uidLength;
+    return _uidLength;
 }
 
 void NfcTag::getUid(uint8_t* uid, uint8_t uidLength)
 {
-	memcpy(_uid, uid, uidLength);
+    memcpy(_uid, uid, uidLength);
 }
 
 String NfcTag::getUidString()
 {
-	String uidString = "";
-	for (int i = 0; i < _uidLength; i++)
-	{
-		if (i > 0) 
-		{ 
-			uidString += " "; 
-		}
+    String uidString = "";
+    for (int i = 0; i < _uidLength; i++)
+    {
+        if (i > 0) 
+        { 
+            uidString += " "; 
+        }
 
-		if (_uid[i] < 0xF) 
-		{
-			uidString += "0"; 
-		}
+        if (_uid[i] < 0xF) 
+        {
+            uidString += "0"; 
+        }
 
-		uidString += String(_uid[i], HEX);
-	}
-	uidString.toUpperCase();
-	return uidString;
+        uidString += String(_uid[i], HEX);
+    }
+    uidString.toUpperCase();
+    return uidString;
 }
 
 String NfcTag::getTagType()
 {
-	return _tagType;
+    return _tagType;
 }
 
 boolean NfcTag::hasNdefMessage() 
@@ -103,19 +103,19 @@ boolean NfcTag::hasNdefMessage()
 
 NdefMessage NfcTag::getNdefMessage()
 {
-	return *_ndefMessage;
+    return *_ndefMessage;
 }
 
 void NfcTag::print()
 {
-	Serial.print(F("NFC Tag - "));Serial.println(_tagType);
-	Serial.print(F("UID "));Serial.println(getUidString());
-	if (_ndefMessage == NULL) 
-	{
+    Serial.print(F("NFC Tag - "));Serial.println(_tagType);
+    Serial.print(F("UID "));Serial.println(getUidString());
+    if (_ndefMessage == NULL) 
+    {
         Serial.println(F("\nNo NDEF Message"));
     }
     else
     {
-	    _ndefMessage->print();    	
-	}
+        _ndefMessage->print();      
+    }
 }
