35d930e1124e2ab19b9e84042aae902f5be8cf47
Don Coleman <dcoleman@chariotsolutions.com>
2013-03-18 00:41:18 -0400

    tabs to spaces

--- NfcTag.h
+++ NfcTag.h
@@ -7,28 +7,28 @@
 
 class NfcTag
 {
-	public:
-		NfcTag();
-		NfcTag(uint8_t* uid, uint8_t uidLength);
-		NfcTag(uint8_t* uid, uint8_t uidLength, String tagType);
-		NfcTag(uint8_t* uid, uint8_t uidLength, String tagType, NdefMessage& ndefMessage);
-		NfcTag(uint8_t* uid, uint8_t uidLength, String tagType, const uint8_t * ndefData, const int ndefDataLength);  
-		~NfcTag(void);
-		NfcTag& operator=(const NfcTag& rhs);
-		uint8_t getUidLength();
-		void getUid(uint8_t* uid, uint8_t uidLength);
-		String getUidString();
-		String getTagType();
+    public:
+        NfcTag();
+        NfcTag(uint8_t* uid, uint8_t uidLength);
+        NfcTag(uint8_t* uid, uint8_t uidLength, String tagType);
+        NfcTag(uint8_t* uid, uint8_t uidLength, String tagType, NdefMessage& ndefMessage);
+        NfcTag(uint8_t* uid, uint8_t uidLength, String tagType, const uint8_t * ndefData, const int ndefDataLength);  
+        ~NfcTag(void);
+        NfcTag& operator=(const NfcTag& rhs);
+        uint8_t getUidLength();
+        void getUid(uint8_t* uid, uint8_t uidLength);
+        String getUidString();
+        String getTagType();
         boolean hasNdefMessage();
-		NdefMessage getNdefMessage();
-		void print();
-	private:
-		uint8_t* _uid;
-		uint8_t _uidLength;
-		String _tagType; // Mifare Classic, NFC Forum Type {1,2,3,4}, Unknown
-		NdefMessage* _ndefMessage;
-		// TODO capacity
-		// TODO isFormatted
+        NdefMessage getNdefMessage();
+        void print();
+    private:
+        uint8_t* _uid;
+        uint8_t _uidLength;
+        String _tagType; // Mifare Classic, NFC Forum Type {1,2,3,4}, Unknown
+        NdefMessage* _ndefMessage;
+        // TODO capacity
+        // TODO isFormatted
 };
 
 #endif
