f069db9749c03b3072314ee82f1dd6eff26f7aa2
Tom Igoe <tom.igoe@gmail.com>
2013-06-30 10:04:36 -0400

    Updated ReadTagExtended again

--- examples/ReadTagExtended/ReadTagExtended.ino
+++ examples/ReadTagExtended/ReadTagExtended.ino
@@ -5,53 +5,47 @@
 NfcAdapter nfc = NfcAdapter();
 
 void setup(void) {
-  Serial.begin(9600);
+  Serial.begin(115200);
   Serial.println("NDEF Reader");
   nfc.begin();
 }
 
 void loop(void) {
-  Serial.println("Scan a NFC tag");
-  if (nfc.tagPresent()) {
+  Serial.println("\nScan a NFC tag\n");
+  if (nfc.tagPresent()) 
+  {
     NfcTag tag = nfc.read();
-    if (tag.hasNdefMessage()) { // every tag won't have a message        
+    tag.print();
+
+    if (tag.hasNdefMessage()) // every tag won't have a message        
+    {
       NdefMessage message = tag.getNdefMessage();
+      // message.print();  // message has a print method (this is more like debug)
+
       // cycle through the records, printing some info from each
       int recordCount = message.getRecordCount();
-      for (int m = 0; m < recordCount; m++) 
+      for (int i = 0; i < recordCount; i++) 
       {
-        NdefRecord record = message.getRecord(m);  
-        //NdefRecord record = message[m]; // alternate syntax
-        Serial.print("TNF: ");
+        NdefRecord record = message.getRecord(i);  
+        // NdefRecord record = message[i]; // alternate syntax
+
         Serial.println(record.getTnf()); 
-        Serial.print("Type: ");
         Serial.println(record.getType()); // will be "" for TNF_EMPTY
 
-        // you can't generically get the payload, since the tnf 
-        // and type determine how the payload is decoded:
+        // we can't generically get the payload, since the tnf and type determine how the payload is decoded
         int payloadLength = record.getPayloadLength();
-        Serial.print("Payload Length: ");
-        Serial.println(payloadLength);
-        // once you know the length, make an array for it:
-        byte payload[payloadLength];   
+        uint8_t payload[payloadLength];
         record.getPayload(payload);
-
-        // Convert the byte array to a String:
-        String result = "";
-        for (int c=0; c< payloadLength; c++) {
-          result += (char)payload[c]; 
-        }
-        // print the payload:
-        Serial.print("Payload: ");
-        Serial.println(result);
+        PrintHexChar(payload, payloadLength);
 
         // id is probably blank and will return ""
-        Serial.print("ID: ");
-        Serial.println(record.getId());                
+        String uid = record.getId();
+        if (uid != "") {
+          Serial.println(uid);
+        }                
       }        
     }    
   }
-  delay(3000);    // delay before next read
+  delay(3000);
 }
-
 
