ed639a8b049e9fa819d2cfdfaed0338b579dfa14
Don Coleman <dcoleman@chariotsolutions.com>
2013-08-30 00:09:13 -0400

    improve example

--- examples/ReadTagExtended/ReadTagExtended.ino
+++ examples/ReadTagExtended/ReadTagExtended.ino
@@ -25,40 +25,62 @@ void setup(void) {
 
 void loop(void) {
   Serial.println("\nScan a NFC tag\n");
-  if (nfc.tagPresent()) 
+
+  if (nfc.tagPresent())
   {
     NfcTag tag = nfc.read();
-    tag.print();
+    Serial.println(tag.getTagType());
+    Serial.print("UID: ");Serial.println(tag.getUidString());
 
-    if (tag.hasNdefMessage()) // every tag won't have a message        
+    if (tag.hasNdefMessage()) // every tag won't have a message
     {
+
       NdefMessage message = tag.getNdefMessage();
-      // message.print();  // message has a print method (this is more like debug)
+      Serial.print("\nThis NFC Tag contains an NDEF Message with ");
+      Serial.print(message.getRecordCount());
+      Serial.print(" NDEF Record");
+      if (message.getRecordCount() != 1) {
+        Serial.print("s");
+      }
+      Serial.println(".");
 
       // cycle through the records, printing some info from each
       int recordCount = message.getRecordCount();
-      for (int i = 0; i < recordCount; i++) 
+      for (int i = 0; i < recordCount; i++)
       {
-        NdefRecord record = message.getRecord(i);  
+        Serial.print("\nNDEF Record ");Serial.println(i+1);
+        NdefRecord record = message.getRecord(i);
         // NdefRecord record = message[i]; // alternate syntax
 
-        Serial.println(record.getTnf()); 
-        Serial.println(record.getType()); // will be "" for TNF_EMPTY
+        Serial.print("  TNF: ");Serial.println(record.getTnf());
+        Serial.print("  Type: ");Serial.println(record.getType()); // will be "" for TNF_EMPTY
 
-        // we can't generically get the payload, since the tnf and type determine how the payload is decoded
+        // The TNF and Type should be used to determine how your application processes the payload
+        // There's no generic processing for the payload, it's returned as a byte[]
         int payloadLength = record.getPayloadLength();
-        uint8_t payload[payloadLength];
+        byte payload[payloadLength];
         record.getPayload(payload);
+
+        // Print the Hex and Printable Characters
+        Serial.print("  Payload (HEX): ");
         PrintHexChar(payload, payloadLength);
+
+        // Force the data into a String (might work depending on the content)
+        // Real code should use smarter processing
+        String payloadAsString = "";
+        for (int c = 0; c < payloadLength; c++) {
+          payloadAsString += (char)payload[c];
+        }
+        Serial.print("  Payload (as String): ");
+        Serial.println(payloadAsString);
 
         // id is probably blank and will return ""
         String uid = record.getId();
         if (uid != "") {
-          Serial.println(uid);
-        }                
-      }        
-    }    
+          Serial.print("  ID: ");Serial.println(uid);
+        }
+      }
+    }
   }
   delay(3000);
 }
-
