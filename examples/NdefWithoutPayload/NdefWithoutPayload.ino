92a411646eeeed47efb9f0125e900485d25e26f9
Charlie Matlack <charlie@potavida.com>
2017-04-09 15:49:38 -0700

    Updates (untested) to ntag class.
    Work in progress.

--- examples/NdefWithoutPayload/NdefWithoutPayload.ino
+++ examples/NdefWithoutPayload/NdefWithoutPayload.ino
@@ -13,55 +13,78 @@
 #include <NdefRecord.h>
 #include <Bounce2.h>
 
+#include <Ndef.h>
+
+#define FD_PIN    (4)
+#define VOUT_PIN  (2)
+#define LED_PIN (13)
+
+// Troubleshooting
+ISR(BADISR_vect)
+{
+    for (;;) UDR0='!';
+}
+
+Ntag ntag(Ntag::NTAG_I2C_2K, FD_PIN, VOUT_PIN);
+
 
 void setup(){
-    Serial.begin(115200);
+    pinMode(13, OUTPUT);
+    Serial.begin(9600);
     Serial.println("start");
-
-    NdefMessage message = NdefMessage();
-
+    
     // Create an NDEF record with a dummy payload and examine header
-    NdefRecord rec = NdefRecord(80);
-    Serial.print("ID length: ");
-    Serial.println(rec.getIdLength());
+    NdefRecord rec = NdefRecord(3);
     rec.print();
 
     unsigned int header_len = rec.getHeaderSize();
     byte header[header_len];
     rec.getHeader(header, true, true);
-    Serial.println(F("\n Record header: "));
+    Serial.println(F("\nRecord header: "));
     showBlockInHex(header, header_len);
 
-    Serial.print(F("\n Size of record in memory: "));
+    Serial.print(F("\nSize of record in memory: "));
     Serial.println(sizeof(rec));
 
+    Serial.print(F("\nPayload size: "));
+    Serial.println(rec.getPayloadLength());
+    Serial.print(F("\nEncoded size of record: "));
+    Serial.println(rec.getEncodedSize());
+
+    
+    NdefMessage message = NdefMessage();
     if (message.addRecord(rec)) { Serial.println(F("Added record.")); }
     unsigned int msgHeader_len = message.getHeaderSize();
     byte msgHeader[msgHeader_len];
     message.getHeader(msgHeader);
-    Serial.println("\n Message header: ");
+    Serial.println(F("\nMessage header: "));
     showBlockInHex(msgHeader, msgHeader_len);
 
-    Serial.print("Record count: ");
+    Serial.print(F("Record count: "));
     Serial.println(message.getRecordCount());
     
-    Serial.print(F("\n Size of message in memory: "));
+    Serial.print(F("\nSize of message in memory: "));
     Serial.println(sizeof(message));
 
-    Serial.println(F("\n (Adding more copies of record to message)"));
-    if (message.addRecord(rec)) { Serial.println(F("Added record.")); }
-    if (message.addRecord(rec)) { Serial.println(F("Added record.")); }
+    Serial.print(F("Message encoded size (excluding NDEF TLV): "));
+    Serial.println(message.getEncodedSize());
 
-    Serial.print("Record count: ");
-    Serial.println(message.getRecordCount());
-    msgHeader_len = message.getHeaderSize();
-    byte msgHeader2[msgHeader_len];
-    message.getHeader(msgHeader2);
-    Serial.println("\n Message header: ");
-    showBlockInHex(msgHeader2, msgHeader_len);
-
-    Serial.print("\n Size of message in memory: ");
-    Serial.println(sizeof(message));
+    delay(500);
+    // Now we'll actually write to an NTAG
+    Serial.print("Zeroing memory...");
+    if (ntag.zeroEeprom()) { Serial.println("success."); }
+    Serial.println("Configuring NTAG...");
+    if (ntag.setContainerClass()) { Serial.println("success."); }          // Configure NTAG for an NDEF message to occupy all user EEPROM
+    Serial.println("Writing NDEF message...");
+    if (ntag.writeNdef(16, message, true))  { Serial.println("success."); } // Write the message to EEPROM (which starts at global address 16)
+    
+    byte block[16];
+    ntag.readConfigBlock(block);
+    Serial.println(F("\nConfig block: "));
+    showBlockInHex(block, 16);
+    Serial.println(F("\nUser EEPROM first block: "));
+    ntag.readEeprom(16, block, 16);
+    showBlockInHex(block, 16);
 }
 
 void loop() {
@@ -69,10 +92,12 @@ void loop() {
 
 }
 
+
 void showBlockInHex(byte* data, byte size){
     for(int i=0;i<size;i++){
         Serial.print(data[i],HEX);
         Serial.print(" ");
+        _delay_ms(10);
     }
     Serial.println();
 }
